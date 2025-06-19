7b15660c8d0ca3b5ea630fa293a23b082e251387
J-JD <jamesjohndoe@protonmail.com>
2025-06-19 17:48:03 +0200

    prop: Selección de elementos del HTML (DOM)
    Guarda referencias a elementos del HTML
    para poder modificarlos

--- src/services.js
+++ src/services.js
@@ -1,2 +1,8 @@
 // 1. Configuración inicial: URL de la API.
 const API_URL = "http://localhost:3000/libros";
+
+// 2. Selección de elementos del HTML (DOM)
+const lista = document.getElementById("listaLibros");
+const formulario = document.getElementById("formulario");
+const btnCancelar = document.getElementById("btnCancelar");
+const tituloFormulario = document.getElementById("tituloFormulario");
